e1e9144800889d1b2394b3622c2a2e7877f39815
chirag7792 <chirag1992dhawan@gmail.com>
2021-10-28 21:10:39 -0400

    my first angular app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { AboutComponent } from './MyComponents/about/about.component';
+
+  const routes: Routes = [
+    { path: '', component: TodosComponent },
+    { path: 'About', component: AboutComponent },
+  ];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './MyComponents/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
